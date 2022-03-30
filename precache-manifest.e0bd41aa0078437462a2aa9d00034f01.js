f15ae5f2e4b87285d7f7b0d06e823db7e8b2feb9
Avi Dalal <avi413@gmail.com>
2022-03-30 23:11:31 +0300

    Updates

---
+++ precache-manifest.e0bd41aa0078437462a2aa9d00034f01.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "59c3a6f6f5c5f3599e09",
+    "url": "/around-react/static/css/main.56e7a96a.chunk.css"
+  },
+  {
+    "revision": "59c3a6f6f5c5f3599e09",
+    "url": "/around-react/static/js/main.59c3a6f6.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "/around-react/static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "48d8704a8192d3af511b",
+    "url": "/around-react/static/js/2.48d8704a.chunk.js"
+  },
+  {
+    "revision": "6f140ef9aa6ae10422f1",
+    "url": "/around-react/static/js/runtime~main.6f140ef9.js"
+  },
+  {
+    "revision": "3e6893cd2eab107d2d5385c921e95382",
+    "url": "/around-react/static/media/logo.3e6893cd.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/around-react/static/media/profile-edit.d476a8b8.svg"
+  },
+  {
+    "revision": "58430b3532ccf0b58af98981a0fb6d48",
+    "url": "/around-react/static/media/Close-Icon.58430b35.svg"
+  },
+  {
+    "revision": "587ea55e6317ff1b4fcba35e837be311",
+    "url": "/around-react/static/media/mobile-close-icon.587ea55e.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/around-react/static/media/edit.58afbe73.svg"
+  },
+  {
+    "revision": "0fa1c3696d696f562fdb745f4ef13d9e",
+    "url": "/around-react/static/media/edit-mobile.0fa1c369.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/around-react/static/media/plus.bdfb8ebb.svg"
+  },
+  {
+    "revision": "ef38b085db5a81756d4dc5a64c1fccb8",
+    "url": "/around-react/static/media/like-active.ef38b085.svg"
+  },
+  {
+    "revision": "f202ad3053ae4410e6d246994df87581",
+    "url": "/around-react/static/media/Trash.f202ad30.svg"
+  },
+  {
+    "revision": "94185013a6a56d2102a4e27feab0b94d",
+    "url": "/around-react/static/media/like.94185013.svg"
+  },
+  {
+    "revision": "eba360005eef21ac6807e45dc8422042",
+    "url": "/around-react/static/media/Inter-Regular.eba36000.ttf"
+  },
+  {
+    "revision": "1aa99aa25c72307cb7f16fae35e8c9d1",
+    "url": "/around-react/static/media/Inter-Medium.1aa99aa2.ttf"
+  },
+  {
+    "revision": "c6dacb6bcfcd747bba440bf2fbd2c85a",
+    "url": "/around-react/static/media/Inter-Black.c6dacb6b.ttf"
+  },
+  {
+    "revision": "b8e0ae293bca366000740a6283d70887",
+    "url": "/around-react/index.html"
+  }
+];
