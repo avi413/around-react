d6797d13db08c71683e4efb37b42fb0bf23e99b7
Avi Dalal <avi413@gmail.com>
2022-04-24 12:11:15 +0300

    destructuring assignment

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 function ImagePopup(props) {
-  const open = props.selectedCard.isCardOpen ? " popup_opened" : "";
+  const { selectedCard, click, close } = props;
+  const isOpen = selectedCard.isCardOpen ? " popup_opened" : "";
   return (
-    <div className={`popup popup_type_img ${open}`} onClick={props.click}>
+    <div className={`popup popup_type_img ${isOpen}`} onClick={click}>
       <div className="popup__img-container">
         <button
           aria-label="close popup"
           className="popup__close popup__close_place_img button button_opacity_m"
-          onClick={props.close}
+          onClick={close}
         />
         <img
-          src={props.selectedCard.link}
+          src={selectedCard.link}
           alt="popup img"
           className="popup__img"
         />
-        <p className="popup__img-title">{props.selectedCard.title}</p>
+        <p className="popup__img-title">{selectedCard.title}</p>
       </div>
     </div>
   );
